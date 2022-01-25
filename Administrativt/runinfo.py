f90f86aa3f32974aec7f446dc7e2ed85572924e1
Cecilie Glittum <cecilie-gl@hotmail.com>
2022-01-25 21:58:26 +0100

    Fixed runinfo.

--- Administrativt/runinfo.py
+++ Administrativt/runinfo.py
@@ -1,6 +1,6 @@
 import os
 
-print("RUN                L       Nh      tl       tr       Jzl       Jzr       Jpml       Jplr")
+print("RUN           L       Nh      tl       tr       Jzl       Jzr       Jpml       Jplr")
 
 directories = [d for d in os.listdir(os.getcwd()) if (d[0:3] == 'Run' and os.path.isdir(d))]
 N = len(directories)
@@ -11,9 +11,13 @@ while folders < N:
     if folder >= 0 and folder < 10:
         if os.path.exists("Run00" + str(folder)):
             outfile = open("Run00" + str(folder) + "/parameters.txt", 'r')
-            words = outfile.readline().split()
-            print("Run00" + str(folder) + "     %15s    %4s    %4s    %4s    %+2.2f    %+2.2f    %+2.2f    %+2.2f"\
-             % (words[0], words[1], words[2], words[3], float(words[4]), float(words[5]), float(words[6]), float(words[7])))
+            words = []
+            for line in outfile:
+                linewords = line.split()
+                if (len(linewords) > 0):
+                    words.append(linewords[-1])
+            print("Run00" + str(folder) + "     %4s    %4s      %+2.2f    %+2.2f    %+2.2f     %+2.2f     %+2.2f      %+2.2f"\
+             % (words[0], words[1], float(words[2]), float(words[3]), float(words[4]), float(words[5]), float(words[6]), float(words[7])))
             folder += 1
             folders += 1
         else:
@@ -22,9 +26,13 @@ while folders < N:
     elif folder > 9 and folder < 100:
         if os.path.exists("Run0" + str(folder)):
             outfile = open("Run0" + str(folder) + "/parameters.txt", 'r')
-            words = outfile.readline().split()
-            print("Run0" + str(folder) + "     %15s    %4s    %4s    %4s    %+2.2f    %+2.2f    %+2.2f    %+2.2f"\
-             % (words[0], words[1], words[2], words[3], float(words[4]), float(words[5]), float(words[6]), float(words[7])))
+            words = []
+            for line in outfile:
+                linewords = line.split()
+                if (len(linewords) > 0):
+                    words.append(linewords[-1])
+            print("Run0" + str(folder) + "     %4s    %4s      %+2.2f    %+2.2f    %+2.2f     %+2.2f     %+2.2f      %+2.2f"\
+             % (words[0], words[1], float(words[2]), float(words[3]), float(words[4]), float(words[5]), float(words[6]), float(words[7])))
             folder += 1
             folders += 1
         else:
@@ -34,9 +42,13 @@ while folders < N:
     elif folder < 1000:
         if os.path.exists("Run" + str(folder)):
             outfile = open("Run" + str(folder) + "/parameters.txt", 'r')
-            words = outfile.readline().split()
-            print("Run" + str(folder) + "     %15s    %4s    %4s    %4s    %+2.2f    %+2.2f    %+2.2f    %+2.2f"\
-             % (words[0], words[1], words[2], words[3], float(words[4]), float(words[5]), float(words[6]), float(words[7])))
+            words = []
+            for line in outfile:
+                linewords = line.split()
+                if (len(linewords) > 0):
+                    words.append(linewords[-1])
+            print("Run" + str(folder) + "     %4s    %4s      %+2.2f    %+2.2f    %+2.2f     %+2.2f     %+2.2f      %+2.2f"\
+             % (words[0], words[1], float(words[2]), float(words[3]), float(words[4]), float(words[5]), float(words[6]), float(words[7])))
             folder += 1
             folders += 1
         else:
